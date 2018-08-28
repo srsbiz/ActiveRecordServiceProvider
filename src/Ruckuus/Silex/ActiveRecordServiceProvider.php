6541f42f64b2be332d78ff2847b74be0acfafbcd
Radosław Kowalewski <radoslaw.kowalewski@gt.pl>
2018-08-28 15:53:53 +0200

    Silex 2

--- src/Ruckuus/Silex/ActiveRecordServiceProvider.php
+++ src/Ruckuus/Silex/ActiveRecordServiceProvider.php
@@ -14,13 +14,20 @@ class ActiveRecordServiceProvider implements ServiceProviderInterface
     function register(Application $app){
         $this->app = $app;
 
-        $app['ar.init'] = $app->share(function (Application $app) {
+        $app['ar.init'] = function (Application $app) {
             \ActiveRecord\Config::initialize(function ($cfg) use ($app) {
                 $cfg->set_model_directory($app['ar.model_dir']);
                 $cfg->set_connections($app['ar.connections']);
                 $cfg->set_default_connection($app['ar.default_connection']);
+				if ($app['debug'] && isset($app['ar.logger'])) {
+					$cfg->set_logging(true);
+					$cfg->set_logger($app['ar.logger']);
+				}
+				if (isset($app['ar.cache'])) {
+					$cfg->set_cache($app['ar.cache'], isset($app['ar.cache_options']) ? $app['ar.cache_options'] : []);
+				}
             });
-        });
+        };
     }
 
     function boot(Application $app){
